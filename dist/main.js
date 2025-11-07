84b6f4be561fbc0e58e8690d83f647c2a221a39d
Oleh Radchenko <or435654@gmail.com>
2025-11-08 00:05:05 +0400

    Przerobienie wersji z hihu na ts

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,22 @@
-console.log("Hello, Worlaaad!");
+const imageMaxBlock = document.getElementById("imageMaxiBlock");
+const maxiImg = document.getElementById("maxiImg");
+const captionText = document.getElementById("caption");
+const closeBtn = document.querySelector(".close");
+document.querySelectorAll(".mini").forEach(img => {
+    const image = img;
+    image.addEventListener("click", () => {
+        imageMaxBlock.style.display = "block";
+        maxiImg.src = image.src.replace("_mini", "");
+        captionText.textContent = image.alt;
+        maxiImg.alt = image.alt;
+    });
+});
+closeBtn.addEventListener("click", () => {
+    imageMaxBlock.style.display = "none";
+});
+imageMaxBlock.addEventListener("click", (e) => {
+    if (e.target === imageMaxBlock)
+        imageMaxBlock.style.display = "none";
+});
 export {};
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
-console.log("Hello, Worlaaad!");
+const imageMaxBlock = document.getElementById("imageMaxiBlock") as HTMLElement;
+const maxiImg = document.getElementById("maxiImg") as HTMLImageElement;
+const captionText = document.getElementById("caption") as HTMLElement;
+const closeBtn = document.querySelector(".close") as HTMLElement;
+
+document.querySelectorAll(".mini").forEach(img => {
+    const image = img as HTMLImageElement;
+    image.addEventListener("click", () => {
+    imageMaxBlock.style.display = "block";
+    maxiImg.src = image.src.replace("_mini", "");
+    captionText.textContent = image.alt;
+    maxiImg.alt = image.alt;
+    });
+});
+
+closeBtn.addEventListener("click", () => {
+    imageMaxBlock.style.display = "none";
+});
+
+imageMaxBlock.addEventListener("click", (e : MouseEvent) => {
+    if (e.target as HTMLElement === imageMaxBlock) imageMaxBlock.style.display = "none";
+});
